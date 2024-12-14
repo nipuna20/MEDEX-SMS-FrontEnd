42d8908aa42acfd0f1d7ca4e39aaa859ea230fdd
nipuna20 <nipunau739@gmail.com>
2024-12-14 22:18:53 +0530

    mm

--- src/Pages/StudentCertificate.js
+++ src/Pages/StudentCertificate.js
@@ -1,14 +1,42 @@
 import React from "react";
-import { Box, Typography, Container } from "@mui/material";
+import { Box, Typography, Container, AppBar, Toolbar } from "@mui/material";
 import QRCode from "react-qr-code";
 
 export default function StudentCertificate() {
     const qrCodeValue = "https://certificate-smoky-theta.vercel.app/"; // Replace with your target URL
 
     return (
-        <Container maxWidth="md">
-            <Box sx={{ textAlign: "center", marginTop: 4 }}>
-                <Typography variant="h6">Scan the QR Code to Download:</Typography>
+        <Box
+            sx={{
+                display: "flex",
+                flexDirection: "column",
+                minHeight: "100vh",
+            }}
+        >
+            {/* Header */}
+            <AppBar position="static" sx={{ backgroundColor: "#003366" }}>
+                <Toolbar>
+                    <Typography variant="h6" sx={{ flexGrow: 1 }}>
+                        MEDEX Academy of Pharmacy
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+
+            {/* QR Code Section */}
+            <Container
+                maxWidth="md"
+                sx={{
+                    flex: "1",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    textAlign: "center",
+                }}
+            >
+                <Typography variant="h6" sx={{ marginBottom: 2 }}>
+                    Scan the QR Code to Download:
+                </Typography>
                 <Box
                     sx={{
                         padding: 2,
@@ -22,7 +50,22 @@ export default function StudentCertificate() {
                 <Typography sx={{ marginTop: 2 }}>
                     After scanning, you will be redirected to the download page.
                 </Typography>
+            </Container>
+
+            {/* Footer */}
+            <Box
+                sx={{
+                    backgroundColor: "#003366",
+                    color: "white",
+                    textAlign: "center",
+                    padding: "10px 0",
+                    position: "relative",
+                }}
+            >
+                <Typography variant="body2">
+                    &copy; 2024 MEDEX Academy of Pharmacy. All Rights Reserved.
+                </Typography>
             </Box>
-        </Container>
+        </Box>
     );
 }
